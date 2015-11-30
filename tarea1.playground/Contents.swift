b658ad7439bc018a8fcb00d54ef971fe2d69337a
nnarria <nnarria@gmail.com>
2015-11-30 13:06:14 -0300

    Primer commit
    Solución de tarea considerando que el cero es par y que las reglas no
    son excluyeres

---
+++ tarea1.playground/Contents.swift
@@ -0,0 +1,24 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+
+var rango = 0...100
+
+for i in rango {
+    if (i%5) == 5 {
+        print ("\(i) Bingo!!!")
+    }
+    
+    if (i%2) == 0 {
+        print ("\(i) par!!!")
+    }
+    else {
+        print ("\(i) impar!!!")
+    }
+    
+    if i >= 30 && i <= 40 {
+        print ("\(i) Viva Swift!!!")
+    }
+    
+}
